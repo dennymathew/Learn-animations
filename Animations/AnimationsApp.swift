2fb77d8660e90e63d151ea1209a474eced27a0cc
Denny Mathew <devdenmathew@gmail.com>
2020-11-22 10:46:43 +0530

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Denny Mathew on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
